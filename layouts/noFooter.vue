7fcd5ddd4c204cbb5bf8518b7eb9bb5d10dc6b6a
DESKTOP-ENRHNU6\User <cutecat8110@gmail.com>
2024-01-08 16:52:15 +0800

    fix(header): z-index 調整,修正點不到 header

--- layouts/noFooter.vue
+++ layouts/noFooter.vue
@@ -2,7 +2,6 @@
   <div class="noFooter-wrapper bg-background">
     <PageHeader class="fixed-top z-1" status="onlyLogo" />
     <NuxtImg class="fixed-top z-0 noFooter-img" src="/img/register.png" />
-
     <div class="noFooter-content d-flex justify-content-center align-items-center z-0 z-md-2">
       <slot />
     </div>
@@ -19,12 +18,12 @@
   .noFooter-content {
     position: relative;
     min-height: 100vh;
-    padding-top: 120px;
-    padding-left: 50vw;
+    margin-top: 120px;
+    margin-left: 50vw;
 
     @include md {
-      padding-top: 72px;
-      padding-left: 0;
+      margin-top: 72px;
+      margin-left: 0;
     }
   }
 
