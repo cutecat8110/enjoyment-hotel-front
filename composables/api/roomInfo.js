8cb421fd5c7decda21642165136fc3afd8d31386
STARCLINIC\tori.lin <tori.lin@starnic.com.tw>
2024-01-18 11:02:49 +0800

    feat(http): method: get 增加 Immediate 參數

--- composables/api/roomInfo.js
+++ composables/api/roomInfo.js
@@ -1,9 +1,8 @@
 import Http from '~/composables/api/http'
 
 const roomInfoAPI = {
-  roomInfo: (id, options) => {
-    console.log('id, options: ', id, options)
-    return Http.get(`api/v1/rooms/${id}`, options)
+  getRoomInfo: (id, options) => {
+    return Http.get(`/api/v1/rooms/${id}`, options, true)
   }
 }
 
--- pages/rooms.vue
+++ pages/rooms.vue
@@ -1,6 +1,6 @@
 <template>
   <PageDefault>
-    <NuxtLink to="/room/123">房型詳細頁</NuxtLink>
+    <NuxtLink to="/room/65a77277d044dc8f856c0a52">房型詳細頁</NuxtLink>
   </PageDefault>
 </template>
 
