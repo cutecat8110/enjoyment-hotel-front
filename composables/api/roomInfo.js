d5ba47a8127f382232949a4d436b05f121a446ad
chunghuing <chung_huing@yahoo.com.tw>
2024-01-25 14:19:38 +0800

    fix: 🐛 調整客房旅宿API

--- composables/api/roomInfo.js
+++ composables/api/roomInfo.js
@@ -1,6 +1,9 @@
 import Http from '~/composables/api/http'
 
 const roomInfoAPI = {
+  getRooms: (options) => {
+    return Http.get(`/api/v1/rooms/`, options)
+  },
   getRoomInfo: (id, options) => {
     return Http.get(`/api/v1/rooms/${id}`, options)
   }
--- pages/roomtype.vue
+++ pages/roomtype.vue
@@ -33,12 +33,6 @@
           <div class="carousel-item active">
             <div class="banner-img d-block w-100"></div>
           </div>
-          <!-- <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="..." />
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="..." />
-          </div> -->
         </div>
 
         <div class="banner-content position-absolute d-lg-flex justify-content-lg-between">
@@ -62,9 +56,111 @@
           <div class="fs-6 fs-md-8 fw-bold col-12 room-choose">房型選擇</div>
           <div class="fs-1 fs-md-3 fw-bold text-primary col-12">各種房型，任您挑選</div>
         </div>
+        <!-- test star-->
+        <div class="row" v-for="(room, index) in roomsTmpl" :key="index">
+          <div class="col-12">
+            <div class="card mb-3 mb-md-4 card-outer">
+              <div class="row g-0" >
+                <div class="col-12 col-lg-7">
+                  <div id="demo" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                      <button
+                        v-for="(list, i) in room.imageUrlList"
+                        :key="`${i}_${list}`"
+                        :class="`${i === 0 ? 'active' : ''}`"
+                        type="button"
+                        :aria-current="`${i === 0}`"
+                        :aria-label="`Slide${i + 1}`"
+                        :data-bs-slide-to="`${i}`"
+                        data-bs-target="#demo"
+                      >
+                      </button>
+                      <!-- <button
+                        type="button"
+                        aria-label="Slide 2"
+                        :data-bs-slide-to="1"
+                        :data-bs-target="`${room.name}`"
+                      ></button>
+                      <button
+                        type="button"
+                        aria-label="Slide 3"
+                        data-bs-slide-to="2"
+                        :data-bs-target="'room.name'"
+                      ></button>
+                      <button
+                        type="button"
+                        aria-label="Slide 4"
+                        data-bs-slide-to="3"
+                        :data-bs-target="'room.name'"
+                      ></button>
+                      <button
+                        type="button"
+                        aria-label="Slide 5"
+                        data-bs-slide-to="4"
+                        :data-bs-target="'room.name'"
+                      ></button> -->
+                    </div>
+                    <div class="carousel-inner" >
+                      <div class="carousel-item ratio ratio-16x9" v-for="(list, i) in room.imageUrlList" :key="i" :class="i === 0 ? 'active': ''">
+                        <img :src="`${list}`" alt="">
+                      </div>
+                    </div>
+                    <button
+                      class="carousel-control-prev"
+                      type="button"
+                      data-bs-slide="prev"
+                      data-bs-target="#demo"
+                    >
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button
+                      class="carousel-control-next"
+                      type="button"
+                      data-bs-slide="next"
+                      data-bs-target="#demo"
+                    >
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </button>
+                  </div>
+                </div>
+
+                <div class="col-12 col-lg-5">
+                  <div class="card-body room-content">
+                    <p class="fs-2 fs-md-3 mb-md-2">{{ room.name }}</p>
+                    <p class="fs-7 fs-md-8">{{  room.description }}</p>
+
+                    <div class="room-set d-flex">
+                      <div class="room-set-detail">
+                        <div class="icon-space"></div>
+                        <p class="fs-8 fs-md-7">{{ room.areaInfo }}</p>
+                      </div>
+                      <div class="room-set-detail">
+                        <Icon class="icon-bed" name="ic:round-king-bed" />
+                        <p class="fs-8 fs-md-7">{{ room.bedInfo }}</p>
+                      </div>
+                      <div class="room-set-detail">
+                        <Icon class="icon-people" name="ic:round-person" />
+                        <p class="fs-8 fs-md-7">2-4 人</p>
+                      </div>
+                    </div>
+
+                    <dir class="room-line"></dir>
+                    <div class="room-price d-flex justify-content-between align-items-center">
+                      <p>NT$ {{ room.price }}</p>
+                      <Icon class="icon-forward" name="ic:round-arrow-forward" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- test end -->
 
         <!-- 尊爵 -->
-        <div class="row">
+        <!-- <div class="row">
           <div class="col-12">
             <div class="card mb-3 mb-md-4 card-outer">
               <div class="row g-0">
@@ -125,7 +221,7 @@
                       class="carousel-control-prev"
                       type="button"
                       data-bs-slide="prev"
-                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-target="#carouselLord"
                     >
                       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                       <span class="visually-hidden">Previous</span>
@@ -134,7 +230,7 @@
                       class="carousel-control-next"
                       type="button"
                       data-bs-slide="next"
-                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-target="#carouselLord"
                     >
                       <span class="carousel-control-next-icon" aria-hidden="true"></span>
                       <span class="visually-hidden">Next</span>
@@ -174,10 +270,10 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> -->
 
         <!-- 景觀 -->
-        <div class="row">
+        <!-- <div class="row">
           <div class="col-12">
             <div class="card mb-3 mb-md-4 card-outer">
               <div class="row g-0">
@@ -238,7 +334,7 @@
                       class="carousel-control-prev"
                       type="button"
                       data-bs-slide="prev"
-                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-target="#carouselLandscape"
                     >
                       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                       <span class="visually-hidden">Previous</span>
@@ -247,7 +343,7 @@
                       class="carousel-control-next"
                       type="button"
                       data-bs-slide="next"
-                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-target="#carouselLandscape"
                     >
                       <span class="carousel-control-next-icon" aria-hidden="true"></span>
                       <span class="visually-hidden">Next</span>
@@ -287,10 +383,10 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> -->
 
         <!-- 豪華 -->
-        <div class="row">
+        <!-- <div class="row">
           <div class="col-12">
             <div class="card mb-3 mb-md-4 card-outer">
               <div class="row g-0">
@@ -351,7 +447,7 @@
                       class="carousel-control-prev"
                       type="button"
                       data-bs-slide="prev"
-                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-target="#carouselLuxury"
                     >
                       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                       <span class="visually-hidden">Previous</span>
@@ -360,7 +456,7 @@
                       class="carousel-control-next"
                       type="button"
                       data-bs-slide="next"
-                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-target="#carouselLuxury"
                     >
                       <span class="carousel-control-next-icon" aria-hidden="true"></span>
                       <span class="visually-hidden">Next</span>
@@ -400,10 +496,10 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> -->
 
         <!-- 尊榮 -->
-        <div class="row">
+        <!-- <div class="row">
           <div class="col-12">
             <div class="card mb-3 mb-md-4 card-outer">
               <div class="row g-0">
@@ -464,7 +560,7 @@
                       class="carousel-control-prev"
                       type="button"
                       data-bs-slide="prev"
-                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-target="#carouselHonor"
                     >
                       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                       <span class="visually-hidden">Previous</span>
@@ -473,7 +569,7 @@
                       class="carousel-control-next"
                       type="button"
                       data-bs-slide="next"
-                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-target="#carouselHonor"
                     >
                       <span class="carousel-control-next-icon" aria-hidden="true"></span>
                       <span class="visually-hidden">Next</span>
@@ -513,13 +609,50 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> -->
       </div>
     </article>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const roomsTmpl = ref<
+{
+  name: string
+  description: string
+  imageUrl:string
+  imageUrlList: string[]
+  areaInfo: string
+  bedInfo: string
+  maxPeople: string
+  price: string
+  _id:string
+}[]
+>([])
+
+/* API */
+const { getRooms } = useApi()
+const apiPending = computed(() => roomsPending.value)
+
+const {
+  pending: roomsPending,
+  refresh: roomsRefresh
+} = await getRooms({
+  immediate: false,
+  onResponse({ response }: { response: any }) {
+    if (response.status === 200) {
+      const temp = response._data.result
+      console.log('response._data.result:', response._data.result)
+      roomsTmpl.value = temp
+    }
+  },
+  onResponseError({ response }: { response: any }) {
+    console.log('getRooms api Error:', response)
+  }
+})
+roomsRefresh()
+
+</script>
 
 <style lang="scss" scoped>
 .banner-img-mask {
@@ -722,8 +855,7 @@
         margin-bottom: 40px;
 
         .room-set-detail {
-          width: 97px;
-          // height: 97px;
+          width: 100px;
           margin-right: 16px;
           padding: 16px;
           border-radius: 8px;
@@ -976,7 +1108,7 @@
           margin-bottom: 24px;
 
           .room-set-detail {
-            width: 97px;
+            width: 100px;
             height: 97px;
             margin-right: 16px;
             padding: 16px;
@@ -1093,36 +1225,6 @@
         background-image: url('/img/mobile/roomtype1-5.png');
       }
 
-      .landscape-img-one {
-        width: 100%;
-        height: 200px;
-        background-image: url('/img/mobile/roomtype2-1.png');
-      }
-
-      .landscape-img-two {
-        width: 100%;
-        height: 200px;
-        background-image: url('/img/mobile/roomtype2-2.png');
-      }
-
-      .landscape-img-three {
-        width: 100%;
-        height: 200px;
-        background-image: url('/img/mobile/roomtype2-3.png');
-      }
-
-      .landscape-img-four {
-        width: 100%;
-        height: 200px;
-        background-image: url('/img/mobile/roomtype2-4.png');
-      }
-
-      .landscape-img-five {
-        width: 100%;
-        height: 200px;
-        background-image: url('/img/mobile/roomtype2-5.png');
-      }
-
       .luxury-img-one {
         width: 100%;
         height: 200px;
@@ -1192,7 +1294,7 @@
           margin-bottom: 24px;
 
           .room-set-detail {
-            width: 97px;
+            width: 100px;
             height: 97px;
             margin-right: 16px;
             padding: 16px;
