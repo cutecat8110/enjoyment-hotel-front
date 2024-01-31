6dedd76f5aa008272189a8ab38761e86aba6d043
chunghuing <chung_huing@yahoo.com.tw>
2024-01-31 15:12:23 +0800

    fix: 🐛 調整composables/api/order.js

--- composables/api/reserve.js
+++
@@ -1,9 +0,0 @@
-import Http from '~/composables/api/http'
-
-const reserveAPI = {
-    submitOrderApi: (options) => {
-        return Http.post('/api/v1/orders/', options)
-    }
-}
-
-export default reserveAPI
