09b48858267eea5c169084dcb778c6cb3c176fa5
chunghuing <chung_huing@yahoo.com.tw>
2024-02-07 15:17:45 +0800

    fix: 🐛 解決合併衝突

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "default" | "h-bg-f-spl" | "h-bg-f" | "h-f-spl" | "h-logo-f-no" | "h-logo-f-spl" | "none"
-declare module "../../node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
-  }
-}
